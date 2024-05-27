7e5d78c9b3790cdb85b898517cf2c8e162bc91b0
Suganth <suganthchandran07@gmail.com>
2024-05-27 21:52:51 +0530

    Updated

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 let answer = document.getElementById("answer");
 
-function disp(input)
-{
-    answer.value +=  input;
+function disp(input) {
+    answer.value += input;
 }
 
-function displayClear()
-{
+function displayClear() {
     answer.value = "";
 }
 
@@ -14,15 +12,22 @@ function singleClear() {
     answer.value = answer.value.slice(0, -1);
 }
 
-function calculate()
-{
-    try
-    {
-        let expression = answer.value.replace(/(^|[^0-9])0+(?=[0-9])/g, '$1');
+function calculate() {
+    try {
+        let expression = answer.value;
+        
+        if (/[\+\-\*\/]{2,}/.test(expression)) {
+            answer.value = "Error";
+            return;
+        }
+
+        if (/[\+\-\*\/]$/.test(expression)) {
+            expression = expression.slice(0, -1);
+        }
+
+        expression = expression.replace(/(^|[^0-9])0+(?=[0-9])/g, '$1'); // Removing leading zeros
         answer.value = eval(expression);
-    }
-    catch(error)
-    {
+    } catch (error) {
         answer.value = "Error";
     }
 }
